4567a3f9c690d0d9e86ff89e6c2444acd23d4086
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-01-25 00:04:12 -0500

    Catch errors in bot methods

--- index.js
+++ index.js
@@ -50,86 +50,112 @@ bot.start(ctx =>
     .then(() =>
       ctx.reply(add)
     )
+    .catch(r => console.log('Bot failed to start\n' + r))
 )
 
 bot.help(ctx => ctx.replyWithHTML(help))
 
 bot.command('next', async ctx => {
-  const userId = ctx.from.id
-  ctx.reply(`Sit tight...I'm fetching your next LeetCode problem to practice 😎`)
-  const question = await getNextQuestion(userId)
-  await ctx.reply('Here is you next question. Practice makes perfect 💪')
-  ctx.reply(questionURL(question.titleSlug))
+  try {
+    const userId = ctx.from.id
+    ctx.reply(`Sit tight...I'm fetching your next LeetCode problem to practice 😎`)
+    const question = await getNextQuestion(userId)
+    await ctx.reply('Here is you next question. Practice makes perfect 💪')
+    ctx.reply(questionURL(question.titleSlug))
+  } catch (e) {
+    console.error('Bot failed to process command /next\n' + e)
+  }
 })
 
 bot.on('message', async ctx => {
-  const userId = ctx.from.id
-  const questionId = Number(ctx.message.text)
-  if (isNaN(questionId) || !Number.isInteger(questionId)) {
-    ctx.reply('Well, this is not a valid question id...Try again?')
-  } else {
-    const question = await getQuestion(questionId)
-    const userQuestion = await getUserQuestion(questionId, userId)
+  try {
+    const userId = ctx.from.id
+    const questionId = Number(ctx.message.text)
 
-    if (userQuestion) {
-      ctx.session.userQuestion = userQuestion
-      await ctx.replyWithHTML(`I've found this LeetCode problem in your question bucket.
-You can <b>update</b> it with the recent status of your solution or <b>delete</b> it from your bucket.`)
-      ctx.reply(questionURL(question.titleSlug), Markup.inlineKeyboard([
-        Markup.button.callback('Delete', 'delete'),
-        Markup.button.callback('Update', 'update')
-      ]))
+    if (isNaN(questionId) || !Number.isInteger(questionId)) {
+      ctx.reply('Well, this is not a valid question id...Try again?')
     } else {
-      const res = await addUserQuestion(questionId, userId)
-      if (res) {
-        await ctx.reply(`Okay, I've added the following LeetCode problem to your question bucket 👍`)
-        ctx.reply(questionURL(question.titleSlug))
+      const question = await getQuestion(questionId)
+      const userQuestion = await getUserQuestion(questionId, userId)
+
+      if (userQuestion) {
+        ctx.session.userQuestion = userQuestion
+        await ctx.replyWithHTML(`I've found this LeetCode problem in your question bucket.
+You can <b>update</b> it with the recent status of your solution or <b>delete</b> it from your bucket.`)
+        ctx.reply(questionURL(question.titleSlug), Markup.inlineKeyboard([
+          Markup.button.callback('Delete', 'delete'),
+          Markup.button.callback('Update', 'update')
+        ]))
       } else {
-        ctx.reply(`Oops...Some error occurred so I couldn't add your question. Try again?`)
+        const res = await addUserQuestion(questionId, userId)
+        if (res) {
+          await ctx.reply(`Okay, I've added the following LeetCode problem to your question bucket 👍`)
+          ctx.reply(questionURL(question.titleSlug))
+        } else {
+          ctx.reply(`Oops...Some error occurred so I couldn't add your question. Try again?`)
+        }
       }
     }
+  } catch (e) {
+    console.error('Bot failed to process user message\n' + e)
   }
 })
 
 bot.action('update', async ctx => {
-  await ctx.answerCbQuery()
-  ctx.editMessageReplyMarkup(Markup.inlineKeyboard([
-    Markup.button.callback('Rejected', 'rejected'),
-    Markup.button.callback('Accepted', 'accepted')
-  ]).reply_markup)
+  try {
+    await ctx.answerCbQuery()
+    ctx.editMessageReplyMarkup(Markup.inlineKeyboard([
+      Markup.button.callback('Rejected', 'rejected'),
+      Markup.button.callback('Accepted', 'accepted')
+    ]).reply_markup)
+  } catch (e) {
+    console.error(`Bot failed to process callback 'update'\n` + e)
+  }
 })
 
 bot.action('delete', async ctx => {
-  const res = await deleteUserQuestion(ctx.session.userQuestion._id)
-  if (res) {
-    ctx.answerCbQuery('Delete successful')
-    ctx.editMessageText(`[Deleted] ${ctx.callbackQuery.message.text}`)
-  } else {
-    ctx.answerCbQuery('Delete failed')
+  try {
+    const res = await deleteUserQuestion(ctx.session.userQuestion._id)
+    if (res) {
+      ctx.answerCbQuery('Delete successful')
+      ctx.editMessageText(`[Deleted] ${ctx.callbackQuery.message.text}`)
+    } else {
+      ctx.answerCbQuery('Delete failed')
+    }
+  } catch (e) {
+    console.error(`Bot failed to process callback 'delete'\n` + e)
   }
 })
 
 bot.action('accepted', async ctx => {
-  const res = await updateRecall(ctx.session.userQuestion, 1)
-  if (res) {
-    ctx.answerCbQuery('Update successful')
-    ctx.editMessageText(`[Accepted] ${ctx.callbackQuery.message.text}`)
-  } else {
-    ctx.answerCbQuery('Update failed')
+  try {
+    const res = await updateRecall(ctx.session.userQuestion, 1)
+    if (res) {
+      ctx.answerCbQuery('Update successful')
+      ctx.editMessageText(`[Accepted] ${ctx.callbackQuery.message.text}`)
+    } else {
+      ctx.answerCbQuery('Update failed')
+    }
+  } catch (e) {
+    console.error(`Bot failed to process callback 'accepted'\n` + e)
   }
 })
 
 bot.action('rejected', async ctx => {
-  const res = await updateRecall(ctx.session.userQuestion, 0)
-  if (res) {
-    ctx.answerCbQuery('Update successful')
-    ctx.editMessageText(`[Rejected] ${ctx.callbackQuery.message.text}`)
-  } else {
-    ctx.answerCbQuery('Update failed')
+  try {
+    const res = await updateRecall(ctx.session.userQuestion, 0)
+    if (res) {
+      ctx.answerCbQuery('Update successful')
+      ctx.editMessageText(`[Rejected] ${ctx.callbackQuery.message.text}`)
+    } else {
+      ctx.answerCbQuery('Update failed')
+    }
+  } catch (e) {
+    console.error(`Bot failed to process callback 'rejected'\n` + e)
   }
 })
 
-bot.launch()
+bot.launch().catch(r => 'Bot failed to lunch\n' + r)
 
 // Enable graceful stop
 process.once('SIGINT', () => {
